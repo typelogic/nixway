bbbd716729e448cf5e790887850de47882ffd397
truth <truthadjustr@gmail.com>
2019-07-28 18:11:13 +0800

    A more clearer nix snippet

--- shell.nix
+++ shell.nix
@@ -1,9 +1,9 @@
 { nixpkgs ? import <nixpkgs> {} }:
 let
   inherit (nixpkgs) pkgs;
-  nixPackages = [pkgs.hello pkgs.lcov pkgs.poco pkgs.gtest pkgs.clang-tools];
+  xxx = [pkgs.hello pkgs.lcov pkgs.poco pkgs.gtest pkgs.clang-tools];
 in
   pkgs.stdenv.mkDerivation {
     name = "nixway";
-    buildInputs = nixPackages;
+    buildInputs = xxx;
   }
